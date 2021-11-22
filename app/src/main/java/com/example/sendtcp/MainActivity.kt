85583dbe6b40fc97948f723352ba8cc773e7e4be
mbp13 <ky7245>
2021-11-22 14:08:12 +0900

    受信メッセージの表示機能を追加

--- app/src/main/java/com/example/sendtcp/MainActivity.kt
+++ app/src/main/java/com/example/sendtcp/MainActivity.kt
@@ -3,53 +3,77 @@ package com.example.sendtcp
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.lang.Exception
 import java.net.InetSocketAddress
 import java.net.Socket
+import kotlin.Exception
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-
     fun clickSendButton(view: View) {
-        val ipAddr = "192.168.1.7"
+        val ipAddr = findViewById<EditText>(R.id.editTextTextIPAddr).text.toString() //"192.168.1.7"
         val portNum = 58080
 
-        val inetSocketAddress = InetSocketAddress(ipAddr, portNum)
+        try {
+            val inetSocketAddress = InetSocketAddress(ipAddr, portNum)
 
-        val task = object : AsyncTask<InetSocketAddress, Void, Void>()  {
-            override fun doInBackground(vararg p0: InetSocketAddress?): Void? {
-                var socket: Socket? = null
-                try {
-                    // connect
-                    socket = Socket()
-                    socket.connect(p0[0])
-                    val writer = BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
+            val task = object : AsyncTask<InetSocketAddress, Void, Void>() {
+                override fun doInBackground(vararg p0: InetSocketAddress?): Void? {
+                    var socket: Socket? = null
+                    try {
+                        // connect
+                        socket = Socket()
+                        socket.connect(p0[0])
+                        val writer = BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
+                        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
 
-                    writer.write("test string.....")
-                    writer.close()
+                        writer.write("test string.....")
+                        writer.flush()
 
-                    val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+                        var received = reader.use { it.readText() }
 
-                    var received = reader.use { it.readText() }
+                        val handler = Handler(Looper.getMainLooper())
+                        handler.post(Runnable {
+                            val mainTextVeiew = findViewById<TextView>(R.id.rcvString)
+                            var oldText = mainTextVeiew?.text
+                            mainTextVeiew?.setText("$oldText \n $received")
+                        })
 
+                        Log.d(TAG, received.toString())
 
-                } catch (e: Exception) {
-                    e.printStackTrace()
+                        writer.close()
+                        reader.close()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                    return null
                 }
-
-                return null
             }
-        }
 
-        task.execute(inetSocketAddress)
+            task.execute(inetSocketAddress)
+        }catch (e:Exception) {
+            // Err Display
+            Snackbar.make(view, "送信エラー", LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        private const val TAG = "MyActivity"
     }
 }
